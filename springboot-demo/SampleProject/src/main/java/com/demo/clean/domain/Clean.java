5b3436cf6e002b951d90d0fda0ed7577844516cf
sangjjangit <getup1214@gmail.com>
2024-05-03 02:34:45 +0900

    clean arch

--- springboot-demo/SampleProject/src/main/java/com/demo/clean/domain/Clean.java
+++ springboot-demo/SampleProject/src/main/java/com/demo/clean/domain/Clean.java
@@ -6,7 +6,19 @@ import lombok.*;
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Clean {
     private String id;
     private String name;
+
+    private String contents;
+    private String title;
+    private String writer;
+
+    @Builder.Default
+    private long created = System.currentTimeMillis();
+
+//    @Builder.Default
+//    private long created = System.currentTimeMillis();
 }
